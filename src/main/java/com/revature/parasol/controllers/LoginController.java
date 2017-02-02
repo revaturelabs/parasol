6ae6a31f1eda7eb4bcd2c038254d573ea6e2af8e
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-02 17:11:05 -0500

    Made spring security work!

--- src/main/java/com/revature/parasol/controllers/LoginController.java
+++ src/main/java/com/revature/parasol/controllers/LoginController.java
@@ -3,94 +3,41 @@
  */
 package com.revature.parasol.controllers;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+import java.security.Principal;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.ParseException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.apache.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Marc
  *
  */
+@EnableOAuth2Sso
 @RestController
-public class LoginController {
+public class LoginController extends WebSecurityConfigurerAdapter{
 
+    @Autowired
+    Force force;
+    
     @RequestMapping("/authenticate")
-    public void loginUser(@RequestParam(value = "username") String username,
-	    @RequestParam(value = "password") String password) {
-
-	// Get an HttpClient for sending the request
-	HttpClient httpclient = HttpClients.createDefault();
-
-	// Build the strings that will make up the request
-	String url = "https://test.salesforce.com/services/oauth2/token";
-	String grantType = "password";
-	String clientId = "";
-	String clientSecret = "";
-
-	String message = "grant_type=" + grantType + "&client_id" + clientId + "&client_secret" + clientSecret
-		+ "&username" + username + "&password" + password;
-
-	String finalUrl = url + "?" + message;
-
-	// Set up the Post method to send
-	HttpPost post = new HttpPost(url);
-
-	List<NameValuePair> params = new ArrayList<NameValuePair>();
-	params.add(new BasicNameValuePair("grant_type", grantType));
-	params.add(new BasicNameValuePair("client_id", clientId));
-	params.add(new BasicNameValuePair("client_secret", clientSecret));
-	params.add(new BasicNameValuePair("username", username));
-	params.add(new BasicNameValuePair("password", password));
-
-	try {
-	    post.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-	} catch (UnsupportedEncodingException e) {
-	    // TODO Auto-generated catch block
-	    Logger.getRootLogger().error(e);
-	}
-
-	//Get the response and parse it to json
-	HttpResponse resp = null;
-	String jsonString="";
-	try {
-	    resp = httpclient.execute(post);
-	    if (resp != null) {
-		HttpEntity entity = resp.getEntity();
-		jsonString = EntityUtils.toString(entity);
-	    }
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    Logger.getRootLogger().error(e);
-	}
-	
-	//Get and parse the json object from the response
-	JSONObject json;
-	try {
-	    json = new JSONObject(jsonString);
-	    json.get("nameOfElement");
-	} catch (JSONException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-
+    public Principal loginUser(OAuth2Authentication principal) {
+	System.out.println(force.printRestUrl(principal));
+	return principal;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .antMatcher("/**")
+        .authorizeRequests()
+          .antMatchers("/", "/login**", "/webjars/**")
+          .permitAll()
+        .anyRequest()
+          .authenticated();
+    }
 }
