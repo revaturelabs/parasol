705b63bab20878677fa16c8f8acfe07573a2e0ab
Vincent <vinduong12@gmail.com>
2017-02-02 18:49:21 -0500

    Login stuff

---
+++ src/main/java/com/revature/parasol/MvcConfig.java
@@ -0,0 +1,10 @@
+package com.revature.parasol;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	//May or may not need this
+}
--- src/main/java/com/revature/parasol/ParasolApplication.java
+++ src/main/java/com/revature/parasol/ParasolApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ParasolApplication {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(ParasolApplication.class, args);
 	}
 }
