95f6052a22ae77eb940aa7f4bcd69318fbce5db0
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-07 11:39:27 -0500

    Still trying to fix angular...

--- src/main/java/com/revature/parasol/SecurityConfig.java
+++ src/main/java/com/revature/parasol/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 	http.antMatcher("/**").authorizeRequests()
-	.antMatchers("/", "/welcome**", "/login**", "/webjars/**")
+	.antMatchers("/", "/welcome**", "/login**", "/webjars/**", "**.js", "**.css")
 		.permitAll()
 	.anyRequest()
 		.authenticated()
