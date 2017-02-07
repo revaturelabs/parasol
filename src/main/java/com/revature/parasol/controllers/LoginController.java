d25a680aed6c2bcdfa0e1ac44a46118df0b74ce9
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-07 15:58:46 -0500

    Tryign to figure out what is in principal

--- src/main/java/com/revature/parasol/controllers/LoginController.java
+++ src/main/java/com/revature/parasol/controllers/LoginController.java
@@ -7,6 +7,7 @@ import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class LoginController{
     @RequestMapping("/login")
     public Principal loginUser(OAuth2Authentication principal) {
 		System.out.println(force.printRestUrl(principal));
+		System.out.println("Principal: " + principal.toString());
 		return principal;
     }
     
