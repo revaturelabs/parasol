c750ac9d9940728ec4127935a9982a68b60b99af
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-07 13:52:30 -0500

    fix?

--- src/main/java/com/revature/parasol/SecurityConfig.java
+++ src/main/java/com/revature/parasol/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 		http.antMatcher("/**").authorizeRequests()
-		.antMatchers("/", "/login**", "/bootstrap/**", "/containter/**", "/jquery/**", "/js/**", "/landing/**")
+		.antMatchers("/", "/login**")
 			.permitAll()
 		.anyRequest()
 			.authenticated()
