523ffd08c3cdc48d6d5f2947845432b3d1fdbff2
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-08 15:06:03 -0500

    Still trying to fix login...

--- src/main/java/com/revature/parasol/controllers/LoginController.java
+++ src/main/java/com/revature/parasol/controllers/LoginController.java
@@ -6,10 +6,9 @@ package com.revature.parasol.controllers;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,7 +22,7 @@ public class LoginController {
     Force force;
 
     @RequestMapping("/login")
-    public Principal loginUser(OAuth2Authentication principal) {
+    public Principal loginUser(@RequestParam(required=false)String code, OAuth2Authentication principal) {
 	System.out.println("Hello world!");
 
 	System.out.println(force.printRestUrl(principal));
