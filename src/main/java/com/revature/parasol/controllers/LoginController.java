fdc882bb6f9d506365db059f088c16ddb07f50d6
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-09 14:47:59 -0500

    ?????

--- src/main/java/com/revature/parasol/controllers/LoginController.java
+++ src/main/java/com/revature/parasol/controllers/LoginController.java
@@ -7,6 +7,7 @@ import java.security.Principal;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
@@ -26,7 +27,7 @@ public class LoginController {
     Force force;
 
     @RequestMapping("/login")
-    public Principal loginUser(@RequestParam(required=false)String code, OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+    public Principal loginUser(@RequestParam(required=false)String code, DefaultOAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 	System.out.println(force.printRestUrl(authentication));
 	System.out.println("Principal: " + authentication.toString() + "\n");
 	System.out.println("Name: " + authentication.getName() + "\n");
