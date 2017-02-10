2448cf498d9de3ebba310aebfab346cfda8cb5d6
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-10 12:14:41 -0500

    Adding an important interface, implementing more useful things in login controller

--- src/main/java/com/revature/parasol/controllers/LoginController.java
+++ src/main/java/com/revature/parasol/controllers/LoginController.java
@@ -5,13 +5,18 @@ package com.revature.parasol.controllers;
 
 import java.security.Principal;
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.parasol.domain.service.RoleModuleServiceInterface;
 
 /**
  * @author Marc
@@ -24,8 +29,37 @@ public class LoginController {
     @Autowired
     Force force;
 
+    @Autowired
+    RoleModuleServiceInterface roleModuleService;
+
     @RequestMapping("/login")
-    public Principal loginUser(@RequestParam(required = false) String code, OAuth2Authentication authentication) {
+    public JSONObject loginUser(@RequestParam(required = false) String code, OAuth2Authentication authentication) {
+
+	//Get the role and modules that the user is allowed to access
+	//I THINK ALL OF THIS NEEDS TO GO ANYWHERE WHERE YOU NEED TO GET THE ROLE OR MODULES
+	OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+	String token = details.getTokenValue();
+	
+	LinkedHashMap<Object, Object> userAuthDetails = (LinkedHashMap<Object, Object>) authentication
+		.getUserAuthentication().getDetails();
+	String userUrl = (String) userAuthDetails.get("sub");
+	
+	String role = roleModuleService.getRoleForUser(userUrl, token);
+	Object moduleList = roleModuleService.getModulesForRole(role);
+	userAuthDetails.put("role", role);
+	userAuthDetails.put("modules", moduleList);
+	
+
+	JSONObject json = new JSONObject();
+	try {
+	    json.put("token", token);
+	    json.put("role", role);
+	    json.put("modules", moduleList);
+	} catch (JSONException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	
 	System.out.println(force.printRestUrl(authentication));
 	System.out.println("Principal: " + authentication.toString() + "\n");
 	System.out.println("Name: " + authentication.getName() + "\n");
@@ -45,9 +79,8 @@ public class LoginController {
 	System.out.println("Map: " + map.toString() + "\n");
 	System.out.println("Key set of map: " + map.keySet().toString() + "\n");
 
-	OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
 	System.out.println("Token: " + details.getTokenValue());
-	
-	return authentication;
+
+	return json;
     }
 }
