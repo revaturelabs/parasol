2f92ae075745c3673e9b171f6b947dec2c8207cd
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-10 16:18:17 -0500

    Finalizing some changes

--- src/main/java/com/revature/parasol/controllers/DefaultRoutingController.java
+++ src/main/java/com/revature/parasol/controllers/DefaultRoutingController.java
@@ -9,17 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DefaultRoutingController {
-    @RequestMapping(value = {"/", "/moduleregistration", "/welcome"} )
+    @RequestMapping(value = {"/", "/moduleregistration", "/index"} )
     public String routeToHome(){
         return "forward:index.html";
     }
-    @Autowired
-    Force force;
-    
-    @RequestMapping("/testing")
-    public String testing(OAuth2Authentication principal){
-	System.out.println(force.printRestUrl(principal));
-	return "test.html";
-    }
-    
 }
