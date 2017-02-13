ce6f99697f5a250cc72ab130a54cce2b2e3ec12d
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-13 09:36:55 -0500

    Fixing issue I hope

--- src/main/java/com/revature/parasol/controllers/LoginController.java
+++ src/main/java/com/revature/parasol/controllers/LoginController.java
@@ -6,25 +6,22 @@ package com.revature.parasol.controllers;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Marc
  *
  */
-@RestController
+@Controller
 @RequestMapping(value = "/auth")
 public class LoginController {
 
@@ -35,8 +32,7 @@ public class LoginController {
     // RoleModuleServiceInterface roleModuleService;
 
     @RequestMapping(value = "/login")
-    public String loginUser(@RequestParam(required = false) String code, OAuth2Authentication authentication,
-	    HttpServletResponse response) throws IOException {
+    public String loginUser(@RequestParam(required = false) String code, OAuth2Authentication authentication) throws IOException {
 
 	// Get the role and modules that the user is allowed to access
 	// I THINK ALL OF THIS NEEDS TO GO ANYWHERE WHERE YOU NEED TO GET THE
@@ -54,9 +50,9 @@ public class LoginController {
 
 	SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-	String forwardString = "forward:index.html" + "?token=" + token;
+	String forwardString = "redirect:index.html" + "?token=" + token;
 
-	response.sendRedirect("https://dev.parasol.revature.pro/index.html?token=" + token);
+	
 	return forwardString;
     }
 
