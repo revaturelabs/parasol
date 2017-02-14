f535a202a8ff3d6c2f428442de00453728300557
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-14 14:20:22 -0500

    Trying to fix js issues

--- src/main/java/com/revature/parasol/SecurityConfig.java
+++ src/main/java/com/revature/parasol/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/login", "/webjars/**")
+				.antMatchers("/", "/login", "/webjars/**", "/public/**")
 					.permitAll()
 				.anyRequest()
 					.authenticated()
