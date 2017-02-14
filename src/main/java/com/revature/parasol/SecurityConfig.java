b0a1942fc0d8990304b0dcd1d660ef8e03abd4da
Marc Kuniansky <mlkuniansky@gmail.com>
2017-02-14 14:35:30 -0500

    This is annoying.

--- src/main/java/com/revature/parasol/SecurityConfig.java
+++ src/main/java/com/revature/parasol/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/login", "/webjars/**", "/public/**")
+				.antMatchers("/", "/login", "/webjars/**")
 					.permitAll()
 				.anyRequest()
 					.authenticated()
