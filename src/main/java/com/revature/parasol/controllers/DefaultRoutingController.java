30ccbc9729f1ddecbbee23974b1eea7f2354661d
Kevin Langhoff <kevinglanghoff@gmail.com>
2017-02-16 12:40:30 -0500

    module registration (fixed checked data

--- src/main/java/com/revature/parasol/controllers/DefaultRoutingController.java
+++ src/main/java/com/revature/parasol/controllers/DefaultRoutingController.java
@@ -1,16 +1,20 @@
 package com.revature.parasol.controllers;
 
-import java.security.Principal;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DefaultRoutingController {
-    @RequestMapping(value = {"/", "/moduleregistration"} )
+    @RequestMapping(value = "/")
     public String routeToHome(){
         return "forward:index.html";
+    }
+    
+    @RequestMapping(value="/moduleRegistration", method=RequestMethod.POST)
+    public String modTest(){
+    	
+    	
+    	
+    	return "hi";
     }
 }
