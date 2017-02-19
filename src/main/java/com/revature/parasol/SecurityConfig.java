3e75f94579a034a4d5937d892cbb6fee7729092c
vinduong12 <vinduong12@gmail.com>
2017-02-18 19:57:44 -0500

    Update SecurityConfig.java
    Getting Rid of login from config

--- src/main/java/com/revature/parasol/SecurityConfig.java
+++ src/main/java/com/revature/parasol/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/login", "/webjars/**")
+				.antMatchers("/", "/webjars/**")
 					.permitAll()
 				.anyRequest()
 					.authenticated()
