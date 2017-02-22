a7b801496f02a005da62e7d088ffca4c6794af0e
Vincent <vinduong12@gmail.com>
2017-02-21 22:54:22 -0500

    Force

--- src/main/java/com/revature/parasol/controllers/Force.java
+++ src/main/java/com/revature/parasol/controllers/Force.java
@@ -95,9 +95,12 @@ public class Force {
 
         try {
             response = new JSONObject(restTemplate.getForObject(url, String.class, params));
+            System.out.println(response);
             int size = response.getInt("totalSize");
             for (int i = 0; i < size; i++) {
-                test.put(response.getJSONArray("records").getJSONObject(i).get("Name"));
+            	JSONObject tmp = new JSONObject();
+            	tmp.put("name", response.getJSONArray("records").getJSONObject(i).getString("Name"));
+                test.put(tmp);
             }
 
         } catch (RestClientException e1) {
