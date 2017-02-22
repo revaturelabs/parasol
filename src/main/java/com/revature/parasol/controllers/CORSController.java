35de6b81a245818874331e6dedf9e87093c0577c
Vincent <vinduong12@gmail.com>
2017-02-21 23:20:55 -0500

    Code cleanup

--- src/main/java/com/revature/parasol/controllers/CORSController.java
+++ src/main/java/com/revature/parasol/controllers/CORSController.java
@@ -24,16 +24,12 @@ public class CORSController {
 
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
 	public Map<String, String> getAllUsers(HttpServletRequest req, OAuth2Authentication p) throws JSONException {
-//		if (req.getCookies() != null) {
-//			System.out.println("Cookies Found " + req.getCookies().length);
-//		}
-
 		//holds the data to be returned
 		Map<String, String> map = new HashMap<>();
 
 		//calls force to get users from SF
 		JSONArray result = force.getAllUsers(p);
-        System.out.println("THE RESULTS " + result.toString());
+
 		//loading map from result
 		map.put("data", result.toString());
 
